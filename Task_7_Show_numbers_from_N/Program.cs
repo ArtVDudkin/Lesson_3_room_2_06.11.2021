7b1acb6073a6fb805d97a8f55f487e0d0a26fafb
Artem <kardan97@yandex.ru>
2021-11-06 19:36:06 +0700

    Task 7 Show numbers from -N to N solved

---
+++ Task_7_Show_numbers_from_N/Program.cs
@@ -0,0 +1,18 @@
+﻿//  7. Показать числа от -N до N
+
+void ShowMeNumbers(int number)                      // функция для вывода на экран чисел от -N до N
+{
+    int startNumber = -number;                      // начальное число равное -N
+    int count = 2* number + 1;                      // определяем, сколько всего чисел от -N до N
+    for (int index = 0; index < count; index++) 
+    {
+        Console.Write($"{startNumber} ");           // выводим текущее число на экран
+        startNumber++;                              // получаем следующее число
+    }
+}
+
+Console.WriteLine("Введите целое число N: ");       // Выводим на экран приглашение ко вводу
+int number = Convert.ToInt32(Console.ReadLine());   // Преобразуем введенное значение в целочисленный тип
+
+ShowMeNumbers(number);                              // Вызываем метод для вывода на экран чисел от -N до N
+Console.WriteLine();
