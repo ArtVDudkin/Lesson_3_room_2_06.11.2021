a00b5454e6bfc6b2551f6e6d127579c5db353a8b
Artem <kardan97@yandex.ru>
2021-11-06 18:26:53 +0700

    Task 1 Square or not solved

--- Task_1_Square_or_not/Program.cs
+++ Task_1_Square_or_not/Program.cs
@@ -1,10 +1,16 @@
 ﻿//  1. По двум заданным числам проверять является ли первое квадратом второго
 
+bool SquareOrNot(int number1, int number2)            // функция для проверки, является ли число 1 квадратом числа 2
+{
+    if (number1 == number2*number2) return true;      // если является, возвращаем истину
+    else return false;                                // если не является, возвращаем ложь
+}
+
 Console.WriteLine("Введите первое число:");            // Выводим на экран приглашение ко вводу
 int number1 = Convert.ToInt32(Console.ReadLine());     // Преобразуем введенное значение в целочисленный тип
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 == number2*number2)                        // Проверяем является введенное значение первого числа квадратом второго числа
-    Console.WriteLine($"Число {number1} является квадратом числа {number2}");    // если да, выводим это сообщение
+if (SquareOrNot(number1, number2) == true)             // Вызываем функцию проверки чисел
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");    // если истина, выводим это сообщение
 else 
-    Console.WriteLine($"Число {number1} не является квадратом числа {number2}"); // если нет, выводим это сообщение
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}"); // если ложь, выводим это сообщение
