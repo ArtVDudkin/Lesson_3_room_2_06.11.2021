1d61af285b3d70a3d1a692044d96ecb8e4e1181c
Artem <kardan97@yandex.ru>
2021-11-06 19:48:45 +0700

    Task 8 Show ordinary numbers from 1 to N solved

---
+++ Task_8_Show_ordinary_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿//  8. Показать четные числа от 1 до N
+
+void ShowMeOrdinaryNumbers(int number)              // функция для вывода на экран чётных чисел от 1 до N
+{
+    int startNumber = 1;                            // начальное число равное 1
+    for (int index = 1; index <= number; index++) 
+    {
+        if (startNumber % 2 == 0)                    // проверяем, если число чётное, то
+            Console.Write($"{startNumber} ");       // выводим его на экран
+        startNumber++;                              // переходим к следующему числу
+    }
+}
+
+Console.WriteLine("Введите целое число N: ");       // Выводим на экран приглашение ко вводу
+int number = Convert.ToInt32(Console.ReadLine());   // Преобразуем введенное значение в целочисленный тип
+
+Console.WriteLine($"Чётные числа от 1 до {number}: ");
+ShowMeOrdinaryNumbers(number);                      // Вызываем метод для вывода на экран чётных чисел от 1 до N
+Console.WriteLine();
