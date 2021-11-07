0bb3ee4684bf14e8880a84e4e0dbcb206d242598
Artem <kardan97@yandex.ru>
2021-11-07 19:58:58 +0700

    Task 7 Show numbers from -N to N solved

--- Task007_ShowNumbersFromN/Program.cs
+++ Task007_ShowNumbersFromN/Program.cs
@@ -1,15 +1,14 @@
 ﻿//  7. Показать числа от -N до N
 
-void ShowMeNumbers(int number)                      // функция для вывода на экран чисел от -N до N
+string ShowMeNumbers(int number)                      // функция для вывода на экран чисел от -N до N
 {                     
-    for (int index = -number; index <= number; index++)  // в цикле от -N до N 
-    {
-        Console.Write($"{index} ");                 // выводим текущее число на экран
-    }
+    return Convert.ToString(number) + " ";            // возвращаем текущее значение number в текстовом виде;                                    // в
 }
 
-Console.WriteLine("Введите целое число N: ");       // Выводим на экран приглашение ко вводу
-int number = Convert.ToInt32(Console.ReadLine());   // Преобразуем введенное значение в целочисленный тип
-
-ShowMeNumbers(number);                              // Вызываем метод для вывода на экран чисел от -N до N
+Console.WriteLine("Введите целое число N: ");         // Выводим на экран приглашение ко вводу
+int number = Convert.ToInt32(Console.ReadLine());     // Преобразуем введенное значение в целочисленный тип
+for (int index = -number; index <= number; index++)   // в цикле от -N до N 
+    {
+        Console.Write(ShowMeNumbers(index));          // выводим текущее число на экран, вызывая метод для вывода чисел от -N до N
+    }
 Console.WriteLine();
