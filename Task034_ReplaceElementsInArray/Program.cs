e2e3665c70c000532a3e2426240f3bd320b03be7
Artem <kardan97@yandex.ru>
2021-11-13 22:48:42 +0700

    Task 34 Replace elements of Array solved

---
+++ Task034_ReplaceElementsInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+int FillArray()                                 // функция для заполнения массива
+{
+    return new Random().Next(-9, 10);           // заполняем элементы массива случайно значениями из [-9,9]
+}
+
+int ReplaceArray(int n)                         // функция для замены элементов на противоположные
+{
+    return -1*n;                                // возвращаем инвертированное значение элемента массива
+}
+
+int[] array = new int[12];                      // создаем массив из 12 элементов
+
+Console.WriteLine("Исходный массив из 12 элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
+Console.WriteLine("Массив с элементами, замененными на противоположные");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = ReplaceArray(array[index]);
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
