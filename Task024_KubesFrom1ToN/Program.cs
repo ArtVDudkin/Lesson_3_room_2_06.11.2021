5008375b52dc2f259f7436b1a7c4fc2eafa2a6e6
Artem <kardan97@yandex.ru>
2021-11-13 18:06:12 +0700

    Task 24 Show kubes from 1 to N solved

--- Task024_KubesFrom1ToN/Program.cs
+++ Task024_KubesFrom1ToN/Program.cs
@@ -3,12 +3,12 @@
 Console.WriteLine("Введите целое число N:");                // Выводим на экран приглашение ко вводу
 int number = Convert.ToInt32(Console.ReadLine());           // Преобразуем введенное значение в целочисленный тип
 
-int Squares(int n)                                          // Метод для расчета кубов
+int Kubes(int n)                                          // Метод для расчета кубов
 {
     return Convert.ToInt32(Math.Pow(n, 3));
 }
 
 for (int index = 1; index <= number; index++)               // для чисел от 1 до N
 {
-    Console.WriteLine($"{index}^3 = {Squares(index)}");     // Выводим на экран таблицу кубов, расчитанную с использованием метода
+    Console.WriteLine($"{index}^3 = {Kubes(index)}");     // Выводим на экран таблицу кубов, расчитанную с использованием метода
 }
