87919dca7e25f46969605b9775fa873e14539dbf
Artem <kardan97@yandex.ru>
2021-11-13 22:33:26 +0700

    Task 33 Array of 12 elements and sum positive solved

---
+++ Task033_ArrayOf12ElementsAndSum/Program.cs
@@ -0,0 +1,22 @@
+﻿// 33. Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+int FillArray()
+{
+    return new Random().Next(0, 10);             // заполняем элементы массива случайно значениями из [0,9]
+}
+
+int[] array = new int[12];                       // создаем массив из 12 элементов
+int positive = 0;
+int negative = 0;
+
+Console.WriteLine("Массив из 12 элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();
+    Console.Write($"{array[index]} ");
+    if (array[index] > 0) positive += array[index];
+    if (array[index] < 0) negative += array[index];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов массива равна {positive}");
+Console.WriteLine($"Сумма отрицательных элементов массива равна {negative}");
