349d68a05e2696885b1357481fc458d74759dbf5
Artem <kardan97@yandex.ru>
2021-11-13 22:25:19 +0700

    Task 32 Array of elements 0 and 1 solved

---
+++ Task032_ArrayOfZeroAndOne/Program.cs
@@ -0,0 +1,16 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int FillArray()
+{
+    return new Random().Next(0, 2);             // заполняем элементы массива случайно значениями из [0,1]
+}
+
+int[] array = new int[8];                       // создаем массив из 8 элементов
+
+Console.WriteLine("Массив из 8 элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
