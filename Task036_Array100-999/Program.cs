50452f9fb196b4be40c3725259436522c566ca69
Artem <kardan97@yandex.ru>
2021-11-13 23:25:38 +0700

    Task 36 Array 100-999 and ordinary-even elements solved

---
+++ Task036_Array100-999/Program.cs
@@ -0,0 +1,24 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int FillArray()                                 // функция для заполнения массива
+{
+    return new Random().Next(100, 1000);        // заполняем элементы массива случайно значениями из [100,999]
+}
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());  
+int[] array = new int[size];                    // создаем массив из size элементов
+int ordinary = 0;
+int even = 0;
+
+Console.WriteLine("Исходный массив элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();                 // заполняем элементы массива случайными числами
+    Console.Write($"{array[index]} ");
+    if (array[index] % 2 == 0) ordinary++;
+    else even++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четный элементов в массиве: {ordinary}");
+Console.WriteLine($"Количество нечетных элементов в массиве: {even}");
