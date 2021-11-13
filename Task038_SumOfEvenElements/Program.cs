a1807a3abbed036ba4b7c597646d4b0f7ef53e0c
Artem <kardan97@yandex.ru>
2021-11-14 00:07:04 +0700

    Task 38 Sum of elements on even positions solved

---
+++ Task038_SumOfEvenElements/Program.cs
@@ -0,0 +1,26 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int FillArray()                                 // функция для заполнения массива
+{
+    return new Random().Next(0, 10);            // заполняем элементы массива случайно значениями из [0,9]
+}
+
+int SumOfEvenElements(int[] array)              // функция для суммирования элементов массива, стоящих на нечетных позициях
+{
+    int sum = 0;
+    for (int index = 0; index < array.Length; index +=2)
+        sum += array[index];
+    return sum;
+}
+
+int size = 20;                                  // задаем размерность массива
+int[] array = new int[size];                    // создаем массив из size элементов
+
+Console.WriteLine("Исходный массив элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();                 // заполняем элементы массива случайными числами
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях, равна: {SumOfEvenElements(array)}");
