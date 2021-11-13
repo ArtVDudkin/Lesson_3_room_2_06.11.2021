dec445074c53051d333623d0ce67346ba1c007ab
Artem <kardan97@yandex.ru>
2021-11-13 22:21:24 +0700

    Task 31 Show array of 8 elements solved

---
+++ Task031_CreateArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+int FillArray()
+{
+    return new Random().Next(1, 10);
+}
+
+int[] array = new int[8];
+
+Console.WriteLine("Массив из 8 элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
