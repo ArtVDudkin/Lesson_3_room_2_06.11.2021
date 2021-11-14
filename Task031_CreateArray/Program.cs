bb505377508591b253404200171bc7bea101c298
Artem <kardan97@yandex.ru>
2021-11-14 15:10:35 +0700

    Task 31 Show array of 8 elements resolved

--- Task031_CreateArray/Program.cs
+++ Task031_CreateArray/Program.cs
@@ -1,16 +1,14 @@
 ﻿// 31. Задать массив из 8 элементов и вывести их на экран
 
-int FillArray()
-{
-    return new Random().Next(1, 10);
+string PrintArray(char[] array)
+{   
+    string result = string.Empty;                                  // Создаем пустую строку 
+    for (int index = 0; index < array.Length; index++)
+        result += $"{Convert.ToChar(new Random().Next(65,90))} ";  // 65-90 это символы A..Z в таблице кодировок
+    return result;
 }
 
-int[] array = new int[8];
+char[] array = new char[8];                                         // создаем массив из 8 символов (по умолчанию пустой)
 
 Console.WriteLine("Массив из 8 элементов");
-for (int index = 0; index < array.Length; index++)
-{
-    array[index] = FillArray();
-    Console.Write($"{array[index]} ");
-}
-Console.WriteLine();
+Console.WriteLine(PrintArray(array));                               // вызываем функцию и выводим на экран массив символов
