513d385559ac460ceeb71edd311fa4903a5c7d1e
Artem <kardan97@yandex.ru>
2021-11-14 10:46:45 +0700

    Task 39 Product of first and last pairs in array solved

---
+++ Task039_ProductOfPairsInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int FillArray()                                 // функция для заполнения массива
+{
+    return new Random().Next(0, 10);            // заполняем элементы массива случайно значениями из [0,9]
+}
+
+int ProductOfPairs(int first, int last)         // функция для расчета произведения пар элементов массива
+{
+    return first*last;
+}
+
+int size = 11;                                  // задаем размерность массива
+int[] array = new int[size];                    // создаем массив из size элементов
+
+Console.WriteLine($"Исходный массив из {array.Length} элементов");
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = FillArray();                 // заполняем элементы массива случайными числами
+    Console.Write($"{array[index]} ");
+}
+Console.WriteLine();
+
+for (int index = 0; index < array.Length / 2; index++)
+{
+    Console.WriteLine($"Произведение пары {array[index]} х {array[array.Length -1 - index]} = {ProductOfPairs(array[index], array[array.Length -1 - index])}");
+}
