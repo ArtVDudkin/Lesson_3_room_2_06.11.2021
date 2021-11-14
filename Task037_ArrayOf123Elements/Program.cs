4891c9014dcde829160e9d36c8352bdc53c5a782
Artem <kardan97@yandex.ru>
2021-11-14 21:06:02 +0700

    Task 37 Array of 123 elements and diapazone of numbers resolved

--- Task037_ArrayOf123Elements/Program.cs
+++ Task037_ArrayOf123Elements/Program.cs
@@ -1,32 +1,51 @@
 ﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
-int FillArray()                                 // функция для заполнения массива
+int[] CreateArray(int count)                    // функция для создания массива
 {
-    return new Random().Next(0, 1000);          // заполняем элементы массива случайно значениями из [0,999]
+    return new int[count];                      // размерности count, по умолчанию заполнен нулями
 }
 
-int FindElement(int[] array, int borderL, int borderR)         // функция для замены элементов на противоположные
+void FillArray(int[] array, int minValue, int maxValue)     // функция для заполнения массива
 {
-    int count = 0;                              // счетчик количества найденых элементов из отрезка
-    for (int index = 0; index < array.Length; index++)
-        if ( (array[index] >= borderL) && (array[index] <= borderR) )
-            count++;                            // если надейно число из отрезка, то увеличиваем счетчик на 1
-    return count;
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);   // заполняем случайными числами от minValue до maxValue
+    }
 }
 
-int[] array = new int[123];                     // создаем массив из 123 элементов
-int borderL = 10;                               // задаем отрезок для поиска значений в диапазоне значений [10,99]  
-int borderR = 99;
-
-Console.WriteLine("Исходный массив из 123 элементов");
-for (int index = 0; index < array.Length; index++)
+string PrintArray(int[] array)                  // функция для вывода массива на экран
 {
-    array[index] = FillArray();                 // заполняем элементы массива случайными числами
-    Console.Write($"{array[index]} ");
+    int count = array.Length;                   
+    string res = String.Empty;                  // создаем пустую строку
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";                  // формируем строку для вывода на экран
+    }
+    return res;
 }
-Console.WriteLine();       
 
-if (FindElement(array, borderL, borderR) == 0)
-    Console.WriteLine($"Элементы из отрезка [{borderL},{borderR}] в массиве не найдены");
-else 
-    Console.WriteLine($"Количество элементов в массиве из отрезка [{borderL},{borderR}] равно {FindElement(array, borderL, borderR)}");
+string FindElement(int[] array, int findMin, int findMax) // функция для замены элементов на противоположные
+{
+    int found = 0;                              // количество найденных значений (по умолчанию 0)
+    int count = array.Length;
+    string result = string.Empty;
+    for (int index = 0; index < count; index++)
+        if ( (array[index] >= findMin) && (array[index] <= findMax) )
+            found++;                            // если найдено число из отрезка, то увеличиваем счетчик на 1
+    
+    if (found == 0)
+        result = $"Элементы из отрезка [{findMin},{findMax}] в массиве не найдены";
+    else 
+        result = $"Количество элементов в массиве из отрезка [{findMin},{findMax}] равно {found}";
+    return result;
+}
+
+Console.Clear();
+int[] arr = CreateArray(123);                   // создаем массив из 123 элементов
+FillArray(arr, 0, 1000);                        // заполняем массив случайными числами от 0 до 999
+Console.WriteLine($"Массив из {arr.Length} элементов");
+Console.WriteLine(PrintArray(arr));             // выводим исходный массив на экран
+int findMin = 10;                               // значения для поиска элементов в диапазоне от 10
+int findMax = 99;                               // до 99
+Console.WriteLine(FindElement(arr, findMin, findMax));    // ищем элементы в массиве и выводим результат поиска на экран
