0a12ab536e4c6fa40ee35c5a46ce0aff1127ebc0
Artem <kardan97@yandex.ru>
2021-11-14 20:23:41 +0700

    Task 35 Find number in array resolved

--- Task035_FindNumberInArray/Program.cs
+++ Task035_FindNumberInArray/Program.cs
@@ -1,36 +1,53 @@
 ﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число
 
-int FillArray()                                 // функция для заполнения массива
+int[] CreateArray(int count)                    // функция для создания массива
 {
-    return new Random().Next(-9, 10);           // заполняем элементы массива случайно значениями из [-9,9]
+    return new int[count];                      // размерности count, по умолчанию заполнен нулями
 }
 
-int FindElement(int[] array, int findN)         // функция для замены элементов на противоположные
+void FillArray(int[] array, int minValue, int maxValue)     // функция для заполнения массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);   // заполняем случайными числами от minValue до maxValue
+    }
+}
+
+string PrintArray(int[] array)                  // функция для вывода массива на экран
+{
+    int count = array.Length;                   
+    string res = String.Empty;                  // создаем пустую строку
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";                  // формируем строку для вывода на экран
+    }
+    return res;
+}
+
+string FindElement(int[] array, int findNumber) // функция для замены элементов на противоположные
 {
     int pos = -1;                               // маркер, найдено ли искомое число
-    for (int index = 0; index < array.Length; index++)
-        if (array[index] == findN) 
+    int count = array.Length;
+    string res = string.Empty;
+    for (int index = 0; index < count; index++)
+        if (array[index] == findNumber) 
         {
             pos = index;                        // если искомое число найдено, запоминаем его индекс
             break;                              // и останавливаем дальнейший поиск
         }
-    return pos;                                 // возвращаем индекс искомого числа в массиве
+    if (pos != -1)                              // если элемент найден, то выводим сообщение
+        res = $"Число {findNumber} присутствует в массиве";
+    else                                        // если элемент не найден, то выводим сообщение
+        res = $"Число {findNumber} отсутствует в массиве";
+    return res;
 }
 
-int[] array = new int[12];                      // создаем массив из 12 элементов
-
-Console.WriteLine("Исходный массив из 12 элементов");
-for (int index = 0; index < array.Length; index++)
-{
-    array[index] = FillArray();                 // заполняем элементы массива случайными числами
-    Console.Write($"{array[index]} ");
-}
-Console.WriteLine();
-
+Console.Clear();
+int[] arr = CreateArray(12);                    // создаем массив из 12 элементов
+FillArray(arr, -10, 10);                        // заполняем массив случайными числами от 0 до 9
+Console.WriteLine($"Массив из {arr.Length} элементов");
+Console.WriteLine(PrintArray(arr));             // выводим исходный массив на экран
 Console.WriteLine("Введите число для поиска в массиве");
-int number = Convert.ToInt32(Console.ReadLine());           
-
-if (FindElement(array, number) != -1)           // если функция вернула значение, отличное от -1, то выводим сообщение, что элемент найден
-    Console.WriteLine($"Число {number} присутствует в массиве");
-else                                            // если функция вернула -1, то выводим сообщение об отсутсвии искомого числа
-    Console.WriteLine($"Число {number} отсутствует в массиве");
+int number = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(FindElement(arr, number));    // ищем элемент в массиве и выводим результат поиска на экран
