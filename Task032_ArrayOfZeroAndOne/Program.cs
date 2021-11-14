04f59d058a1ff87b50bd778a77df63297befd727
Artem <kardan97@yandex.ru>
2021-11-14 19:50:19 +0700

    Task 32 Array of elements 0 and 1 resolved

--- Task032_ArrayOfZeroAndOne/Program.cs
+++ Task032_ArrayOfZeroAndOne/Program.cs
@@ -1,17 +1,32 @@
 ﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
-string FillArray(int[] array)
+int[] CreateArray(int count)                    // функция для создания массива
 {
-    string result = string.Empty;
-    for (int index = 0; index < array.Length; index++)
-    {
-        array[index] = new Random().Next(0, 2);         // заполняем элементы массива случайно значениями из [0,1]
-        result += $"{array[index]} ";
-    }  
-    return result;            
+    return new int[count];                      // размерности count, по умолчанию заполнен нулями
 }
 
-int[] array = new int[8];                               // создаем массив из 8 элементов
+void FillArray(int[] array, int minValue, int maxValue)     // функция для заполнения массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);   // заполняем случайными числами от minValue до maxValue
+    }
+}
 
+string PrintArray(int[] array)                  // функция для вывода массива на экран
+{
+    int count = array.Length;                   
+    string res = String.Empty;                  // создаем пустую строку
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";                  // формируем строку для вывода на экран
+    }
+    return res;
+}
+
+Console.Clear();
+int[] arr = CreateArray(8);                     // создаем массив из 8 элементов
+FillArray(arr, 0, 2);                           // заполняем массив случайными числами [0, 1]
 Console.WriteLine("Массив из 8 элементов");
-Console.WriteLine(FillArray(array));
+Console.WriteLine(PrintArray(arr));             // выводим массив на экран
