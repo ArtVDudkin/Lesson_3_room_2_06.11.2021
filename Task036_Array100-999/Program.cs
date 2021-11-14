5ffd50d61e59dec87a66f46a86c41bb99163e56c
Artem <kardan97@yandex.ru>
2021-11-14 20:44:12 +0700

    Task 36 Array 100-999 and ordinary-even elements resolved

--- Task036_Array100-999/Program.cs
+++ Task036_Array100-999/Program.cs
@@ -1,24 +1,50 @@
 ﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
 
-int FillArray()                                 // функция для заполнения массива
+int[] CreateArray(int count)                    // функция для создания массива
 {
-    return new Random().Next(100, 1000);        // заполняем элементы массива случайно значениями из [100,999]
+    return new int[count];                      // размерности count, по умолчанию заполнен нулями
 }
 
-Console.WriteLine("Введите размерность массива");
-int size = Convert.ToInt32(Console.ReadLine());  
-int[] array = new int[size];                    // создаем массив из size элементов
-int ordinary = 0;
-int even = 0;
-
-Console.WriteLine("Исходный массив элементов");
-for (int index = 0; index < array.Length; index++)
+void FillArray(int[] array, int minValue, int maxValue)     // функция для заполнения массива
 {
-    array[index] = FillArray();                 // заполняем элементы массива случайными числами
-    Console.Write($"{array[index]} ");
-    if (array[index] % 2 == 0) ordinary++;
-    else even++;
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);   // заполняем случайными числами от minValue до maxValue
+    }
 }
-Console.WriteLine();
-Console.WriteLine($"Количество четный элементов в массиве: {ordinary}");
-Console.WriteLine($"Количество нечетных элементов в массиве: {even}");
+
+string PrintArray(int[] array)                  // функция для вывода массива на экран
+{
+    int count = array.Length;                   
+    string res = String.Empty;                  // создаем пустую строку
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";                  // формируем строку для вывода на экран
+    }
+    return res;
+}
+
+string EvenOdd(int[] array, int even)           // функция для нахождения количества четных/нечетных элементов массива
+{
+    int count = array.Length;
+    string result = string.Empty;               // контейнер для итогового сообщения
+    int kolvo = 0;                              // количество найденных элементов
+    for (int i = 0; i < count; i++)
+    {
+        if ((even == 1) && (array[i] % 2 != 0)) kolvo++;    // если параметр even =1, ищем количество нечетных элементов
+        if ((even == 2) && (array[i] % 2 == 0)) kolvo++;    // если параметр even =2, ищем количество четных элементов
+    }
+    if (even == 1) result = $"Количество нечетных элементов в массиве: {kolvo}";
+    if (even == 2) result = $"Количество четных элементов в массиве: {kolvo}";
+    return result;
+}
+
+Console.Clear();
+int[] arr = CreateArray(12);                    // создаем массив из 12 элементов
+FillArray(arr, 100, 1000);                      // заполняем массив случайными числами от [100, 999]
+Console.WriteLine($"Массив из {arr.Length} элементов");
+Console.WriteLine(PrintArray(arr));             // выводим массив на экран
+Console.WriteLine("Введите число для параметр: 1 - для поиска нечетных элементов, 2 - для поиска четных элементов в массиве");
+int param = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(EvenOdd(arr, param));             // Указать параметр 1 - нечетные, 2 - четные
