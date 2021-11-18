3fc50b885bf572c1aa7ab28296e34f39473a2d7c
Artem <kardan97@yandex.ru>
2021-11-18 20:43:05 +0700

    Task 41 Triangle or not solved

--- Task041_TriangleOrNot/Program.cs
+++ Task041_TriangleOrNot/Program.cs
@@ -1,24 +1,19 @@
 ﻿// 41. Выяснить являются ли три числа сторонами треугольника
 
-// функция для проверки образуют три точки треугольник или нет
-string TriangleOrNot(int x1, int y1, int x2, int y2, int x3, int y3) 
-{   // если координаты трех точек различны и не совпадают, то это треугольник. Если координаты каких-либо двух точек совпадают, то это не треугольник, а прямая
-    if (x1 == x2 && y1 == y2 || x1 == x3 && y1 == y3 || x2 == x3 && y2 == y3)   
-        return $"Координаты введенных точек совпадают и не образуют треугольник";
+// функция для проверки могут ли три отрезка образовать треугольник или нет
+string TriangleOrNot(int a, int b, int c) 
+{   // Сумма длин двух любых сторон треугольника больше длины оставшейся стороны:
+    if ( (a + b > c) && (a + c > b) && (b + c > a) )  
+        return $"Введенные три отрезка могут образовать треугольник";
     else
-        return $"Введенные три точки образуют треугольник";
+        return $"Введенные три отрезка не могут образовать треугольник";
 }
 
 Console.Clear();
-Console.WriteLine("Введите координаты точки A(x1,y1):");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты точки B(x2,y2):");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты точки C(x3,y3):");
-int x3 = Convert.ToInt32(Console.ReadLine());
-int y3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите три числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
 // выводим на экран значение функции при заданных значениях координат
-Console.WriteLine(TriangleOrNot(x1, y1, x2, y2, x3, y3)); 
+Console.WriteLine(TriangleOrNot(a, b, c)); 
