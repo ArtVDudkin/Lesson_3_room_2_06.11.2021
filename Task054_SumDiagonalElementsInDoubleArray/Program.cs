004eb9bbcff9550b070559c8759d432af9b67e4f
Artem <kardan97@yandex.ru>
2021-11-28 17:28:28 +0700

    Task 54 Sum of main diagonal elements in 2D array solved

---
+++ Task054_SumDiagonalElementsInDoubleArray/Program.cs
@@ -0,0 +1,49 @@
+﻿// 54. В матрице чисел найти сумму элементов главной диагонали
+
+int[,] CreateArray(int m, int n)
+{
+    return new int [m,n];
+}
+
+void FillArray(int[,] array, int minValue, int maxValue)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+}
+
+int SumDiagonalElements(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) sum += array[i,j];
+        }
+    }
+    return sum;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],2} ");
+        }
+    System.Console.WriteLine();
+    }
+}
+
+int m = 5;
+int n = 5;
+int[,] arrayNew = CreateArray(m,n);
+FillArray(arrayNew, -5, 6);                             // вызываем метод для заполнения массива числами [-5, 5]
+PrintArray(arrayNew);
+Console.WriteLine();
+Console.WriteLine(SumDiagonalElements(arrayNew));
