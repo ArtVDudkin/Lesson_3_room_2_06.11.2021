fc5f80499ab322eb55fbbc2a196184de768b9619
Artem <kardan97@yandex.ru>
2021-12-03 23:01:17 +0700

    Task 68 Recursion show numbers from M to N solved

---
+++ Task068_RecursShowNumbersFromMtoN/Program.cs
@@ -0,0 +1,21 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+
+int ShowNextNumber(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m} ");
+        if (m == n) return n;
+        return ShowNextNumber(m +1, n);
+    }
+    else
+    {
+        Console.Write($"{m} ");
+        if (m == n) return m;
+        return ShowNextNumber(m -1, n);
+    }
+}
+
+Console.Clear();
+ShowNextNumber(20, 30);
+Console.WriteLine();
