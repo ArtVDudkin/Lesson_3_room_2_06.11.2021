058568a3015c6e02958391f492a6bebdcf592ed7
Artem <kardan97@yandex.ru>
2021-12-03 23:29:39 +0700

    Task 70 Recursion find sum of digits solved

---
+++ Task070_RecursSumOfDigits/Program.cs
@@ -0,0 +1,11 @@
+﻿// 70. Найти сумму цифр числа
+
+int SumOfDigits(int number)
+{
+    number = Math.Abs(number);
+    if (number / 10 == 0) return 1;
+    else return number % 10 + SumOfDigits(number / 10);
+}
+
+Console.Clear();
+Console.WriteLine(SumOfDigits(-123456));
