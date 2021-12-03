6ce4cce160dd718e11b5cf328c8fc462fd3561bd
Artem <kardan97@yandex.ru>
2021-12-03 21:19:51 +0700

    Task 67 Recursion show numbers from N to 1 solved

---
+++ Task067_RecursShowNumbersFromNto1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+
+int ShowNextNumber(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1) return 1;
+    else return ShowNextNumber(n -1);
+}
+
+Console.Clear();
+ShowNextNumber(10);
+Console.WriteLine();
