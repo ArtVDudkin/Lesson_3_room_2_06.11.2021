7c3c8c782f67052f02b43bfd5f9c33ae3d9e646f
Artem <kardan97@yandex.ru>
2021-12-03 23:20:36 +0700

    Task 72 Recursion find A in power B solved

---
+++ Task072_RecursAinPowerB/Program.cs
@@ -0,0 +1,10 @@
+﻿// 72. Написать программу возведения числа А в целую стень B
+
+int PowerAB(int number, int power)
+{
+    if (power == 0) return 1;
+    else return number * PowerAB(number, power -1);
+}
+
+Console.Clear();
+Console.WriteLine(PowerAB(-2, 5));
