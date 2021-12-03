641dee6ce343ef166266cb9dc097e553d0da6230
Artem <kardan97@yandex.ru>
2021-12-03 23:12:55 +0700

    Task 69 Recursion find sum elements from M to N solved

---
+++ Task069_RecursSumFromMtoN/Program.cs
@@ -0,0 +1,18 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+
+int SumFromMtoN(int m, int n)
+{
+    if (m < n)
+    {
+        if (m == n) return n;
+        else return m + SumFromMtoN(m +1, n);
+    }
+    else
+    {
+        if (m == n) return n;
+        return m + SumFromMtoN(m -1, n);
+    }
+}
+
+Console.Clear();
+Console.WriteLine(SumFromMtoN(-10, 10));
