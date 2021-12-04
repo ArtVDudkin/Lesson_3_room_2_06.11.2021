08df0d4a40c215b075169ab488adfa76b38149ce
Artem <kardan97@yandex.ru>
2021-12-04 19:40:21 +0700

    Task 73 Recursion Sequence a+b solved

---
+++ Task073_RecursFibo/Program.cs
@@ -0,0 +1,15 @@
+﻿// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//     Первые два элемента последовательности задаются пользователем
+
+string Sequence(int f1, int f2, int n)
+{
+    if (n == 0) return $"";
+    return $"{f1 + f2} {Sequence(f2, f2 + f1, n -1)} ";   //       Sequence(f1 -1, n -1) + Sequence( , n -2); 
+}
+
+Console.Clear();
+Console.WriteLine("Введите первый элемент последовательности");
+int f1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второй элемент последовательности");
+int f2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sequence(f1,f2, 20)); 
