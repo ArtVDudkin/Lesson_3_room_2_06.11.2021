eb0a72913b05643d0acc208cb378458861599fba
Artem <kardan97@yandex.ru>
2021-12-04 21:16:16 +0700

    Task 72 Recursion find A in power B resolved

--- Task072_RecursAinPowerB/Program.cs
+++ Task072_RecursAinPowerB/Program.cs
@@ -6,5 +6,13 @@ int PowerAB(int number, int power)
     else return number * PowerAB(number, power -1);
 }
 
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return a* PowerRecMath(a, n -1);
+}
+
 Console.Clear();
 Console.WriteLine(PowerAB(-2, 5));
+Console.WriteLine(PowerRecMath(-2, 5));
