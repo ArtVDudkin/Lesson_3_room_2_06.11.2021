08c9ed4b2020ae682c5ceabe5eec6387c14dfb28
Artem <kardan97@yandex.ru>
2021-12-05 14:33:39 +0700

    Task 63 Show 3D array solved

--- Task063_Show3DArray/Program.cs
+++ Task063_Show3DArray/Program.cs
@@ -2,7 +2,7 @@
 
 int[, ,] CreateArray(int m, int n, int k)
 {
-    return new int [m,n, k];
+    return new int [m,n,k];
 }
 
 void FillArray(int[, ,] array)
@@ -22,31 +22,24 @@ void FillArray(int[, ,] array)
 
 void PrintArrayInt(int[, ,] array)
 {
-    // for (int m = 0; m < array.GetLength(0); m++)
-    // {
-    //     for (int n = 0; n < array.GetLength(1); n++)
-    //     {   // {array[i, j],2}  добавляем 2 пробела перед значением для красивого вывода отрицательных чисел
-    //         Console.Write($"{array[i, j],2} ");
-    //     }
-    //     Console.WriteLine();
-    // }
-
     for (int m = 0; m < array.GetLength(0); m++)
     {
         for (int n = 0; n < array.GetLength(1); n++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
-                {
-                    Console.WriteLine($"{m} {n} {k} = {array[m,n,k]}");
-                }
+            {
+                Console.WriteLine($"{m} {n} {k} = {array[m,n,k]}");
+            }
+            Console.WriteLine();
         }
+        Console.WriteLine();
     }
 }
 
 Console.Clear();
 int m = 4;
-int n = 3;
-int k = 7;
+int n = 4;
+int k = 4;
 if (m*n*k > 90)
     Console.WriteLine($"Ошибка! Невозможно заполнить трехмерный массив {m}x{n}x{k} неповторяющимися двузначными числами! Введите меньшую размерность массива.");
 else
